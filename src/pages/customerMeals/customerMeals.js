010dbb9d357ca402cda8dc79e2a48dd0c4e1c10a
Pera <alex96pro@gmail.com>
2020-11-11 17:25:42 +0100

    multiple products in cart added

--- src/pages/customerMeals/customerMeals.js
+++ src/pages/customerMeals/customerMeals.js
@@ -3,15 +3,30 @@ import { useDispatch, useSelector } from 'react-redux';
 import './customerMeals.scss';
 import NavBar from '../../components/NavBar/NavBar';
 import MealIcon from '../../images/meal-icon.jpg';
-import {putMealInCart} from '../../common/actions/cart.actions';
+import {putMealInCart, increaseMealAmountInCart} from '../../common/actions/cart.actions';
 
 export default function CustomerMeals(props){
 
     const meals = useSelector(state=>state.customerReducer.meals);
+    const mealsInCart = useSelector(state=>state.cartReducer.meals);
     const dispatch = useDispatch();
 
     const handleAddToCart = (meal) =>{
-        dispatch(putMealInCart(meal))
+        console.log(mealsInCart);
+        let mealExistsInCart = false;
+        let id;
+        for(let i=0;i<mealsInCart.length;i++){
+            if(mealsInCart[i].meal.mealId === meal.mealId){
+                    mealExistsInCart = true;
+                    id = mealsInCart[i].meal.mealId;
+                    break;
+            }
+        }
+        if(mealExistsInCart){                               //meal already exists in cart
+            dispatch(increaseMealAmountInCart(id));
+        }else{                                              //meal doesnt exist in cart
+            dispatch(putMealInCart({meal:meal, amount:1})); 
+        }
     }
     
     return (
@@ -24,6 +39,7 @@ export default function CustomerMeals(props){
                     <div>
                         <div className="meal-name">{meal.name}</div>
                         <div>{meal.description}</div>
+                        <div>{meal.price}$</div>
                         <div><button className="button-add-to-cart" onClick={()=>handleAddToCart(meal)}>Add to cart</button></div>
                     </div>
                     <div>
