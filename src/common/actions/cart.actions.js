b28ec7016ad9f99b56a9edfb7da9b7cb07253bdf
Pera <alex96pro@gmail.com>
2020-11-11 14:07:30 +0100

    added remove button for cart

--- src/common/actions/cart.actions.js
+++ src/common/actions/cart.actions.js
@@ -1,8 +1,14 @@
 export const PUT_MEAL_IN_CART = "PUT_MEAL_IN_CART";
-
+export const REMOVE_MEAL_FROM_CART = "REMOVE_MEAL_FROM_CART";
 export function putMealInCart(payload){
     return {
         type: PUT_MEAL_IN_CART,
+        payload
+    }
+}
+export function removeMealFromCart(payload){
+    return {
+        type: REMOVE_MEAL_FROM_CART,
         payload
     }
 }
--- src/pages/cart/cart.js
+++ src/pages/cart/cart.js
@@ -1,16 +1,26 @@
 import React from 'react';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
 import MealIcon from '../../images/meal-icon.jpg';
 import './cart.scss';
 import NavBar from '../../components/NavBar/NavBar.js';
+import { removeMealFromCart } from '../../common/actions/cart.actions';
+import {Link} from 'react-router-dom';
 export default function Cart(props){
     
     const meals = useSelector(state=>state.cartReducer.meals);
+    const dispatch = useDispatch();
+    const handleRemove = (meal) =>{
+        dispatch(removeMealFromCart(meal));
+    }
     return(
         <div>
             <NavBar isLoggedIn = {true} role={localStorage.getItem("role")}/>
             <div className="cart-meals">
-                <div className="cart-header"><h1>Meals in your cart</h1></div>
+                <div className="cart-header">
+                    {meals.length?<h1>Meals in your cart</h1>:<div>
+                    <h1>You don't have any meals in your cart</h1>
+                    <Link to="/customer"><button className="main-button">Find Cooks</button></Link></div>}
+                </div>
                     {meals.map(
                         meal=>
                         <div className="cart-meal-details">
@@ -19,6 +29,9 @@ export default function Cart(props){
                             </div>
                             <div>
                                 <img src={MealIcon} alt="jej" width="40px" height="40px"/>
+                            </div>
+                            <div>
+                                <button className="remove-from-cart-button" onClick={()=>handleRemove(meal)}>Remove</button>
                             </div>
                         </div>
                     )}
