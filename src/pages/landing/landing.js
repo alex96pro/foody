c12287f8fa64d3e78345207742422cfc3bb6ed85
Pera <alex96pro@gmail.com>
2020-11-30 15:18:44 +0100

    render carousel only when data arrives

--- src/pages/landing/landing.js
+++ src/pages/landing/landing.js
@@ -34,7 +34,8 @@ export default function Landing() {
                 <Link className="landing-button" to='/sign-up' onClick={() => dispatch(pickedRegisterRole('Cook'))}>I want to cook</Link>
             </div>
             <div className="landing-cooks-header">Most popular cooks</div>
-            <Carousel items={featuredCooks}/>
+            {featuredCooks.length &&
+                <Carousel items={featuredCooks}/>}
         </div>
     );
 };
